18b27d54e45f9e74554adb7d819e6414ba3df22c
IVAYLO_GETSOV <ivaylo.getsov.a63@learn.telerikacademy.com>
2025-01-16 17:24:19 +0200

    Added ghost positioning, player positioning and movement

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ const gameBoard = [
     1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 ];
+const keys = {
+    ArrowRight: false, 
+    ArrowLeft: false, 
+    ArrowUp: false, 
+    ArrowDown: false
+};
 const ghosts = [];
 const game = {
     x:'', 
@@ -22,7 +28,7 @@ const game = {
     inplay: false
 }
 const player = {
-    pos:20,
+    pos:84,
     speed: 4,
     cool: 0,
     pause: false
@@ -35,8 +41,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
     game.mouth = document.querySelector('.mouth'); // select pacman mouth
     game.ghost = document.querySelector('.ghost'); // select ghost object
     game.ghost.style.display = 'none';
+    game.packman.style.display = 'none';
     createGame(); // initialize the game board
     console.log(game);
+})
+
+// add ghosts on the board
+document.addEventListener('keydown', (e)=> {
+    console.log(e.code); // Key presses
+    if(e.code in keys){
+        keys[e.code] = true;
+    }
+    if(!game.inplay && !player.pause){
+        game.packman.style.display = 'block';
+        player.play = requestAnimationFrame(move);
+        game.inplay = true;
+    }
+})
+
+document.addEventListener('keyup', (e)=>{
+    if(e.code in keys){
+        keys[e.code] = false;
+    }
 })
 
 
@@ -51,6 +77,10 @@ function createGame(){
     }
     game.grid.style.gridTemplateColumns = game.x;
     game.grid.style.gridTempateRows = game.x;
+
+    for(let i=0;i<game.ghosts;i++){
+        createGhost();
+    }
 }
 
 
@@ -70,4 +100,52 @@ function createSquare(val){
 
     game.grid.append(div);
     myBoard.push(div);
+    div.t = val;
+    div.idVal = myBoard.length;
+}
+
+function createGhost(){
+    let newGhost = game.ghost.cloneNode(true);
+    newGhost.pos = 12 + ghosts.length;
+    newGhost.style.display = 'block';
+    // Iterate board list with colors with each newly added ghost
+    newGhost.style.backgroundColor = board[ghosts.length];
+    newGhost.name = board[ghosts.length] + 'y';
+    ghosts.push(newGhost); 
+    console.log(newGhost);
+}
+
+function move(){
+    if(game.inplay){
+        player.cool--; // player cooldown move speed
+        if(player.cool < 0){
+            // movement of ghosts on screen
+            ghosts.forEach((ghost)=>{
+                myBoard[ghost.pos].append(ghost);
+            })
+
+            // kayboard events
+            let tempPos = player.pos;
+            if(keys.ArrowRight){
+                player.pos+=1;
+            }
+            else if(keys.ArrowLeft){
+                player.pos -=1;
+            }
+            else if(keys.ArrowUp){
+                player.pos -=game.size;
+            }
+            else if(keys.ArrowDown){
+                player.pos +=game.size;
+            }
+            player.cool = player.speed; // set cool off
+            let newPos = myBoard[player.pos]; // new position
+            console.log(newPos);
+        }
+        
+        
+        console.log(player.pos);
+        myBoard[player.pos].append(game.packman);
+        player.play = requestAnimationFrame(move);
+    }
 }
